a80998363fc4668b2d37eedd1e57c5129a79d154
adnanortakoy <ortakoyadnan@gmail.com>
2023-06-13 20:32:23 +0300

    first commit

---
+++ src/test/java/com/dgmarkt/step_definitions/Hooks.java
@@ -0,0 +1,40 @@
+package com.dgmarkt.step_definitions;
+
+
+import com.dgmarkt.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.util.concurrent.TimeUnit;
+
+public class Hooks {
+
+    @Before
+    public void setUp() {
+        System.out.println("\tThis is coming from Before method");
+        Driver.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        Driver.get().manage().window().maximize();
+    }
+
+    @After
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()){
+            final byte[] screenshot=((TakesScreenshot)Driver.get()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png","screenshot");
+        }
+        Driver.closeDriver();
+    }
+
+    @Before ("@db")
+    public void setUpDB(){
+        System.out.println("\t Connection DB");
+    }
+    @After ("@db")
+    public void tearDownDB(){
+        System.out.println("\t Disconnection DB");
+    }
+
+}
