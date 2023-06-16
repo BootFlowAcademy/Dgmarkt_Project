59ef6adb23c4a79229016f95c119bda89b042b60
77ismail <ismailbahri@gmail.com>
2023-06-16 23:12:08 +0300

    16 haziran currency euro-dolar yüklendi

--- src/test/java/com/dgmarkt/pages/BasePage.java
+++ src/test/java/com/dgmarkt/pages/BasePage.java
@@ -8,26 +8,33 @@ import org.openqa.selenium.support.PageFactory;
 
 public abstract class BasePage {
     public BasePage() {
-        PageFactory.initElements(Driver.get(),this);
+        PageFactory.initElements(Driver.get(), this);
     }
+
     @FindBy(xpath = "//a[@title='My Account']/..")
     public WebElement myAccount_btn;
     @FindBy(xpath = "//a[@id='pt-login-link']/..")
     public WebElement accountLogin_btn;
     @FindBy(xpath = "//div[@id='logo']")
     public WebElement homeLogo_icon;
-
     @FindBy(xpath = "//button[@class='btn btn-inverse btn-block btn-lg dropdown-toggle']")
     public WebElement cartIcon_btn;
-
     @FindBy(xpath = "//ul[@class='dropdown-menu pull-right']")
     public WebElement cartProduct_list;
-
     @FindBy(xpath = "(//li[@class='li-top-item left '])[1]")
     public WebElement category_btn;
-
     @FindBy(xpath = "(//div[@class='mega-menu-container sub-menu-container left '])[1]")
     public WebElement categorySubMenu_DropDown;
     @FindBy(id = "wishlist-total")
     public WebElement wishlist_Btn;
+    @FindBy(xpath = "//*[text()='Currency']/..")
+    public WebElement currency_btn;
+    @FindBy(xpath = "//*[contains(text(),' Euro')]/..")
+    public WebElement currencySubMenuEuro_btn;
+    @FindBy(xpath = "//*[contains(text(),' US Dollar')]/..")
+    public WebElement currencySubMenuDollar_btn;
+    @FindBy(xpath = "//*[text()='Currency']/../span[1]")
+    public WebElement currentCurrency_text;
+
+
 }
