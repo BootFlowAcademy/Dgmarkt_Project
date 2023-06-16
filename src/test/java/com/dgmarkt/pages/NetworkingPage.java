53c1a3b18143fb5796c57c288a754bce2ada4b1f
fatih <fatih@gmail.com>
2023-06-16 19:39:11 +0300

    Verify that the Show: tab initially shows "12" is comleted

--- src/test/java/com/dgmarkt/pages/NetworkingPage.java
+++ src/test/java/com/dgmarkt/pages/NetworkingPage.java
@@ -1,9 +1,13 @@
 package com.dgmarkt.pages;
 
+import org.junit.Assert;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
 
 public class NetworkingPage extends BasePage{
     @FindBy(xpath = "(//a[text()='Networking'])[1]")
     public WebElement networking_loc;
-}
+
+    }
+
--- src/test/java/com/dgmarkt/pages/TelevisionsPage.java
+++ src/test/java/com/dgmarkt/pages/TelevisionsPage.java
@@ -1,9 +1,12 @@
 package com.dgmarkt.pages;
 
+import org.junit.Assert;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
 
-public class TelevisionsPage extends BasePage{
-    @FindBy (xpath = "(//a[text()='Televisions'])[1]")
+public class TelevisionsPage extends BasePage {
+    @FindBy(xpath = "(//a[text()='Televisions'])[1]")
     public WebElement television_loc;
+
 }
