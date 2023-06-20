525b4aa7ce1db6595f46ff878c6c49096f3e82f0
NezihIhsanGungor <nezihgungor@gmail.com>
2023-06-21 00:16:06 +0300

    test scenarios created

--- src/test/java/com/dgmarkt/step_definitions/US_16_ProductCompareFunction_StepDefs.java
+++ src/test/java/com/dgmarkt/step_definitions/US_16_ProductCompareFunction_StepDefs.java
@@ -1,42 +1,46 @@
 package com.dgmarkt.step_definitions;
 
+import com.dgmarkt.pages.ComparisonPage;
+import com.dgmarkt.pages.HealthBeautyPage;
+import com.dgmarkt.utilities.BrowserUtils;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+import static com.dgmarkt.utilities.BrowserUtils.scrollToElement;
 
 public class US_16_ProductCompareFunction_StepDefs {
-    @When("The user hover over Category button and click Health&Beauty button")
-    public void the_user_hover_over_category_button_and_click_health_beauty_button() {
 
+    ComparisonPage comparisonPage = new ComparisonPage();
+    HealthBeautyPage healthBeautyPage = new HealthBeautyPage();
+
+    @When("The user hovers the {string} and click {string}")
+    public void the_user_hovers_the_and_click(String Menu, String Submenu) {
+        healthBeautyPage.goToSubmenu(Menu,Submenu);
     }
-    @Then("The user should be able to see Health&Beauty page")
-    public void the_user_should_be_able_to_see_health_beauty_page() {
-
+    @Then("Verify that the user is on Comparison Page")
+    public void verify_that_the_user_is_on_comparison_page() {
+        Assert.assertTrue(comparisonPage.comparisonPage_text.isDisplayed());
+        BrowserUtils.waitFor(2);
     }
-
-    @When("The user hover over Category button and click Televisions button")
-    public void the_user_hover_over_category_button_and_click_televisions_button() {
-
+    @When("The user adds products to the compare page")
+    public void the_user_adds_products_to_the_compare_page() {
+      healthBeautyPage.addToCompare_mtd();
     }
-    @Then("The user should be able to see Televisions page")
-    public void the_user_should_be_able_to_see_televisions_page() {
-
+    @When("The user clicks Product Compare button")
+    public void the_user_clicks_product_compare_button() {
+        scrollToElement(healthBeautyPage.productCompare_btn);
+        BrowserUtils.waitFor(2);
+        healthBeautyPage.productCompare_btn.click();
     }
-
-    @When("The user hover over Category button and click TV Accessories button")
-    public void the_user_hover_over_category_button_and_click_tv_accessories_button() {
-
-    }
-    @Then("The user should be able to see TV Accessories page")
-    public void the_user_should_be_able_to_see_tv_accessories_page() {
-
-    }
-
-    @When("The user hover over Category button and click Networking button")
-    public void the_user_hover_over_category_button_and_click_networking_button() {
-
-    }
-    @Then("The user should be able to see Networking page")
-    public void the_user_should_be_able_to_see_networking_page() {
+    @Then("Verify that the user can see {string} and {string} on the Comparison Page")
+    public void verifyThatTheUserCanSeeAndOnTheComparisonPage(String item1, String item2) {
+        comparisonPage.comparisonPage_text.isDisplayed();
+        scrollToElement(comparisonPage.product_detail_loc);
+        BrowserUtils.waitFor(3);
+        Assert.assertEquals(item1,comparisonPage.item1Health_loc.getText());
+        Assert.assertEquals(item2,comparisonPage.item2Health_loc.getText());
 
     }
 }
--- src/test/resources/features/US_16_ProductCompareFunction.feature
+++ src/test/resources/features/US_16_ProductCompareFunction.feature
@@ -1,7 +1,6 @@
-@BFB1DGMAUT-16
 Feature: Product Compare Function
 
-  Background: The user logs in valid credentials and open homepage
+  Background: : : The user logs in valid credentials and open homepage
     Given The user navigate to web site
     When The user enters login panel credential and clicks login
     And The user closes cookies pop up
@@ -10,18 +9,24 @@ Feature: Product Compare Function
     And The user enters account credentials "nezih@bootflow.academy" and "12345" and clicks login
     Then Verify that the user is on home page
 
-  Scenario: The user should be able to navigate the Product Compare Page when the User clicks on the Product Compare Health&Beauty sub-Tab
-    When The user hover over Category button and click Health&Beauty button
-    Then The user should be able to see Health&Beauty page
+  @BFB1DGMAUT-16
+  Scenario Outline: The user should be able to navigate the Product Compare Page when the User clicks on the Product Compare
+    When The user hovers the "<Menu>" and click "<Submenu>"
+    And The user clicks Product Compare button
+    Then Verify that the user is on Comparison Page
+    Examples:
+      | Menu     | Submenu         |
+      | Category | Health & Beauty |
+      | Category | Televisions     |
+      | Category | TV Accessories  |
+      | Category | Networking      |
 
-  Scenario: The user should be able to navigate the Product Compare Page when the User clicks on the Product Compare Televisions sub-Tab
-    When The user hover over Category button and click Televisions button
-    Then The user should be able to see Televisions page
+  @BFB1DGMAUT-16
+  Scenario: The user should be able to see the Products they want to compare on the Product Comparison page
+    When The user navigates to Health & Beauty category
+    And The user adds products to the compare page
+    And The user clicks Product Compare button
+    Then Verify that the user can see "BaByliss 3663U - Hair rollers" and "Capsule Plate 6pcs" on the Comparison Page
 
-  Scenario: The user should be able to navigate the Product Compare Page when the User clicks on the Product Compare TV Accessories sub-Tab
-    When The user hover over Category button and click TV Accessories button
-    Then The user should be able to see TV Accessories page
+  Scenario: The user receives a warning message when they want to add more than four Products to the Product Comparison page
 
-  Scenario: The user should be able to navigate the Product Compare Page when the User clicks on the Product Compare Networking sub-Tab
-    When The user hover over Category button and click Networking button
-    Then The user should be able to see Networking page
