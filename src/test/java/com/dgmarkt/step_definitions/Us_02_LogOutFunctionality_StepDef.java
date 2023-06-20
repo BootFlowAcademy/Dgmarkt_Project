d5041effa8246f58a913a3628fab44cfbfae579d
yavsev <yavsev97@hotmail.com>
2023-06-20 21:20:35 +0300

    ok

--- src/test/java/com/dgmarkt/step_definitions/Us_02_LogOutFunctionality_StepDef.java
+++ src/test/java/com/dgmarkt/step_definitions/Us_02_LogOutFunctionality_StepDef.java
@@ -1,27 +1,50 @@
 package com.dgmarkt.step_definitions;
 
 import com.dgmarkt.pages.HomePage;
+import com.dgmarkt.pages.LoginPanel;
 import com.dgmarkt.utilities.BrowserUtils;
+import com.dgmarkt.utilities.ConfigurationReader;
+import com.dgmarkt.utilities.Driver;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Us_02_LogOutFunctionality_StepDef {
     HomePage homePage = new HomePage();
-
+    LoginPanel loginPanel=new LoginPanel();
+    String unExpectedWindowId="";
     @When("The user clicks to My Account button")
     public void the_user_clicks_to_my_account_button() {
         homePage.myAccount_btn.click();
     }
     @When("The user clicks to Logout button")
     public void the_user_clicks_to_logout_button() {
-        Assert.assertFalse("Account logout box should not be displayed",homePage.accountLogout_box.isDisplayed());
+        Assert.assertFalse("Account logout box should not be displayed", homePage.accountLogout_box.isDisplayed());
         homePage.logout_btn.click();
-            }
+    }
     @Then("Verify that the user has securely terminated the session and sees the Account Logout box")
     public void verify_that_the_user_has_securely_terminated_the_session_and_sees_the_account_logout_box() {
-        BrowserUtils.waitForVisibility(homePage.accountLogout_box,2);
-        Assert.assertTrue("Account logout box should be displayed",homePage.accountLogout_box.isDisplayed());
-
+        BrowserUtils.waitForVisibility(homePage.accountLogout_box, 2);
+        Assert.assertTrue("Account logout box should be displayed", homePage.accountLogout_box.isDisplayed());
     }
-  }
+    @When("The user clicks to Exit Button \\(X)  for the browser close")
+    public void the_user_clicks_to_exit_button_x_for_the_browser_close() throws InterruptedException {
+        unExpectedWindowId= Driver.get().getWindowHandle();
+        Driver.closeDriver();
+        Thread.sleep(2000); // 2 saniye bekleme süresi
+    }
+    @Then("The user verify that current browser is not old browser")
+    public void the_user_verify_that_current_browser_is_not_old_browser() {
+        Assert.assertNotEquals(unExpectedWindowId,Driver.get().getWindowHandle());
+    }
+    @Then("Verify that the user has securely terminated the session and the login panel page is open")
+    public void verify_that_the_user_has_securely_terminated_the_session_and_the_login_panel_page_is_open() {
+        String actualMessage= Driver.get().findElement(By.xpath("//h5[text()='Please Enter Your Username and Password']")).getText();
+        Assert.assertEquals("Please Enter Your Username and Password",actualMessage);
+    }
+}
+
--- src/test/resources/features/US_02_LogOutFunctionality.feature
+++ src/test/resources/features/US_02_LogOutFunctionality.feature
@@ -1,4 +1,4 @@
-@BFB1DGMAUT-2
+
 Feature: Log out Functionality
 
   Background: The user is on home page
@@ -14,6 +14,28 @@ Feature: Log out Functionality
     And The user clicks to Logout button
     Then Verify that the user has securely terminated the session and sees the Account Logout box
 
+  @BFB1DGMAUT-2
+  Scenario: The user should be able to log out with Exit Button (X)
+    When The user clicks to Exit Button (X)  for the browser close
+    Given The user navigate to web site
+    Then The user verify that current browser is not old browser
+    Then Verify that the user has securely terminated the session and the login panel page is open
 
+  Scenario: The user cannot re-login with "back to page"  icon in browser, if the user has successfully logged out
+    When The user clicks to My Account button
+    And The user clicks to Logout button
+    Then Verify that the user has securely terminated the session and sees the Account Logout box
+    And  The user clicks to Go Back Button
+    Then Verify that the user can not be login by clicking the step back button after successfully logged out
 
+  Scenario: The user should be automatic logout when there is no movement on the site for a certain period of time
+    When The user clicks to Refresh Button after wait 10 minutes when there is no movement on the site
+    Then Verify that the page was refresh
+    And The user clicks to My Account button
+    Then Verify that Automatic logged out of the site for security reasons when there is no movement on the site for a certain period of time
 
+  Scenario: Verify that when we go to a new address from site and click to "click to go back"  icon after 10 minutes, it should not be possible to log in to the account
+    When The user writes in address bar "www.google.com" and press enter
+    Then Verify that the "www.google.com" page opens
+    And The user  waits  "10 minutes" and clicks to "click to go back"  icon
+    Then verify that   https://dgmarkt.com the account is not active
