72e9aec6080045b4a4f1fae46e67485c7a5ca183
hayatic <hayaticalcoban@gmail.com>
2023-06-21 12:08:03 +0300

    Compare Box tested - us22

--- src/test/java/com/dgmarkt/runners/CukesRunner.java
+++ src/test/java/com/dgmarkt/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org
         features = "src/test/resources/features",
         glue = "com/dgmarkt/step_definitions",
         dryRun = false,
-        tags = "@BFB1DGMAUT-22"
+        tags = ""
 )
 public class CukesRunner {
 }
