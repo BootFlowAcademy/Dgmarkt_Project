1767d3e6dc8bd90cec66f13b0f4ccbb5324efa03
ErolIsıksoy <erolisiksoy2@gmail.com>
2023-06-21 20:26:16 +0300

    commit wednesday afternoonnn

--- src/test/java/com/dgmarkt/step_definitions/Us_20_QuickViewIconFunction_StepDef.java
+++ src/test/java/com/dgmarkt/step_definitions/Us_20_QuickViewIconFunction_StepDef.java
@@ -9,28 +9,45 @@ import org.junit.Assert;
 import org.openqa.selenium.interactions.Actions;
 
 public class Us_20_QuickViewIconFunction_StepDef {
-    HomePage homePage = new HomePage();
-    Actions actions = new Actions(Driver.get());
-    @When("The user hovers the cursor over the category tab")
-    public void the_user_hovers_the_cursor_over_the_category_tab() {
-        Assert.assertFalse(homePage.categorySubMenu_DropDown.isDisplayed());
-        actions.moveToElement(homePage.category_btn).perform();//Bu metod Category sekmesinde  HoverOver yapar.
-    }
-    @Then("Verify that the sees  Sub-Category tabs under the Category Module")
-    public void verify_that_the_sees_sub_category_tabs_under_the_category_module() {
-        Assert.assertTrue(homePage.categorySubMenu_DropDown.isDisplayed());
-    }
-    @Given("The user click the Category")
-    public void the_user_click_the_category() {
-        homePage.category_btn.click();
-    }
-    @Then("Verify that the user is on Category Module")
-    public void verify_that_the_user_is_on_category_module() {
-        String expectedUrl = "https://dgmarkt.com/Category";
-        String actualUrl = Driver.get().getCurrentUrl();
-        Assert.assertEquals(expectedUrl, actualUrl);
-    }
-    @Then("Verify that the user is on {string}")
-    public void verify_that_the_user_is_on(String categoryName) {
-        Assert.assertEquals(categoryName, homePage.pageName_text.getText());
-    }
+//    HomePage homePage = new HomePage();
+//    Actions actions = new Actions(Driver.get());
+//    @When("The user hovers the cursor over the category tab")
+//    public void the_user_hovers_the_cursor_over_the_category_tab() {
+//        Assert.assertFalse(homePage.categorySubMenu_DropDown.isDisplayed());
+//        actions.moveToElement(homePage.category_btn).perform();//Bu metod Category sekmesinde  HoverOver yapar.
+//    }
+//    @Then("Verify that the sees  Sub-Category tabs under the Category Module")
+//    public void verify_that_the_sees_sub_category_tabs_under_the_category_module() {
+//        Assert.assertTrue(homePage.categorySubMenu_DropDown.isDisplayed());
+//    }
+//    @Given("The user click the Category")
+//    public void the_user_click_the_category() {
+//        homePage.category_btn.click();
+//    }
+//    @Then("Verify that the user is on Category Module")
+//    public void verify_that_the_user_is_on_category_module() {
+//        String expectedUrl = "https://dgmarkt.com/Category";
+//        String actualUrl = Driver.get().getCurrentUrl();
+//        Assert.assertEquals(expectedUrl, actualUrl);
+//    }
+//    @Then("Verify that the user is on {string}")
+//    public void verify_that_the_user_is_on(String categoryName) {
+//        Assert.assertEquals(categoryName, homePage.pageName_text.getText());
+}
+
+
+@When("The user clicks the {string} Category button")
+public void the_user_clicks_the_category_button(String string) {
+
+        }
+@When("The user hovers over the first product on the {string} page")
+public void the_user_hovers_over_the_first_product_on_the_page(String string) {
+        }
+@When("The user can see first product {string}")
+public void the_user_can_see_first_product(String string) {
+
+        }
+@Then("The quick view icon should be displayed")
+public void the_quick_view_icon_should_be_displayed(){
+
+        }
--- src/test/resources/features/US_20_QuickViewIconFunction.feature
+++ src/test/resources/features/US_20_QuickViewIconFunction.feature
@@ -9,17 +9,44 @@ Feature: Quick View Function
     When The user clicks the login under the my account
     And The user enters account credentials "soyeroli@yahoo.com" and "Kayserili1" and clicks login
     Then Verify that the user is on home page
-
-
-  Scenario: The user hovers over the product, they can see the quick View Icon.
     When The user hovers the cursor over the category tab
     Then Verify that the sees  Sub-Category tabs under the Category Module
-    When The user clicks "<subcategory>" button
-    Then Verify that the user is on "<Sub-CategoryPage>"
-    When The user clicks the "Health & Beauty" Category button
-    And The user hovers over the first product on the "Health & Beauty" page
-    And The user can see first product "BaByliss 3663U - Hair rollers"
-    Then The quick view icon should be displayed
+
+  Scenario Outline: The user hovers over the product, they can see the quick View Icon.
+    When The user clicks on "<CategoryName>", Verify that the user sees "<CategoryNameText>" and scrolls down
+    Then The user hovers one product from every category to the quick view Icon
+    Examples:
+      | CategoryName    | CategoryNameText |
+      | Health & Beauty | Health & Beauty  |
+      | Televisions     | Televisions      |
+      | TV Accessories  | TV Accessories   |
+      | Networking      | Networking       |
+
+  Scenario: The user can browse the product when the user clicks the quick view Icon
+    Given The user clicks one product from every category to the quick view Icon
+    Then Verify the user should see the product page
+
+  Scenario: Adding Product to Wish List from Quick View Popup
+    Given The user clicks the Add to Wish List Icon
+    When The product should be added to the wish list
+    Then Verify that Success: You have added the product to your wish list!
+
+  Scenario: Adding Product to Compare List from Quick View Popup
+    Given The user clicks the compare this product to cart Icon
+    When The product should be added to cart
+    Then Verify that Success: You have added the product to your shopping cart!
+
+  Scenario: Adding Product to Cart from Quick View Popup
+
+    Given The user clicks the Add to cart Icon
+    When The product should be added to cart
+    Then Verify that Success: You have added the product to your shopping cart!
+
+  Scenario: Increasing Product Quantity in Cart from Quick View Popup
+    Given The user is viewing a product in the quick view popup
+    When The user fills in the quantity box to increase the number of products
+    Then The updated quantity should be reflected in the cart
+
 
 
 
