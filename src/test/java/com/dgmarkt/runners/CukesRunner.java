48f47546c813c70a9f33eeb69fee7c5141dd953e
Fatih <yolcu.m.fatih@gmail.com>
2023-06-22 21:51:17 +0300

    Second commit to remote.

--- src/test/java/com/dgmarkt/runners/CukesRunner.java
+++ src/test/java/com/dgmarkt/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org
         features = "src/test/resources/features",
         glue = "com/dgmarkt/step_definitions",
         dryRun = false,
-        tags = ""
+        tags = "@BFB1DGMAUT-21"
 )
 public class CukesRunner {
 }
